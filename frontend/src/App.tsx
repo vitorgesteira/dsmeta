1618c154fc35001b8de10e8db54065d25e2d295c
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2022-07-11 11:11:39 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
- return(
-  <h1>Ola mundo!</h1>
- )
+    return (
+        <>
+            <h1>Ola mundo!</h1>
+            <NotificationButton />
+            <NotificationButton />
+        </>
+    )
 }
 
 export default App
