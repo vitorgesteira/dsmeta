9da6dc4ac6aeb36bc4bce176ac3d5efb30452f9f
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2022-07-11 15:11:40 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/vitor-gesteira/">@vitor-gesteira</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+export default Header
